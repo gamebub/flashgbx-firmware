102d9b9db8713133e26f519d96bc6808dd111819
Eli Lipsitz <eli.lipsitz@gmail.com>
2026-05-13 22:19:54 -0400

    Add Game Bub device implementation for LK firmware

--- Makefile
+++ Makefile
@@ -5,14 +5,16 @@ OBJDUMP = riscv64-unknown-elf-objdump
 ARCH = rv32imc
 ABI = ilp32
 
-CFLAGS = -march=$(ARCH) -mabi=$(ABI) -ffreestanding -nostdlib -O3 -Wall -Iinclude
+CFLAGS = -march=$(ARCH) -mabi=$(ABI) -ffreestanding -nostdlib -O3 -Wall -Iinclude -Iexternal/FlashGBX_LK_Firmware -DLK_DEVICE_HEADER=\"LK_Game_Bub.h\"
 LDFLAGS = -T linker.ld -nostdlib -Wl,--no-warn-rwx-segments
 
 BUILD_DIR = build
 TARGET = $(BUILD_DIR)/firmware
 
 SRC_DIR = src
-SRCS = $(SRC_DIR)/start.S $(SRC_DIR)/main.c
+EXTERNAL_DIR = external
+LK_DIR = $(EXTERNAL_DIR)/FlashGBX_LK_Firmware
+SRCS = $(SRC_DIR)/start.S $(SRC_DIR)/main.c $(LK_DIR)/LK.c
 OBJS = $(addprefix $(BUILD_DIR)/, $(addsuffix .o, $(basename $(notdir $(SRCS)))))
 
 all: $(TARGET).elf $(TARGET).bin $(TARGET).asm
@@ -24,6 +26,9 @@ $(BUILD_DIR)/%.o: $(SRC_DIR)/%.c | $(BUILD_DIR)
 	$(CC) $(CFLAGS) -c $< -o $@
 
 $(BUILD_DIR)/%.o: $(SRC_DIR)/%.S | $(BUILD_DIR)
+	$(CC) $(CFLAGS) -c $< -o $@
+
+$(BUILD_DIR)/%.o: $(LK_DIR)/%.c | $(BUILD_DIR)
 	$(CC) $(CFLAGS) -c $< -o $@
 
 $(BUILD_DIR):
--- src/main.c
+++ src/main.c
@@ -1,5 +1,52 @@
 #include "device.h"
 
+#include "LK.h"
+
+void debug(uint32_t out) {
+    GPIO_PMOD_DIR = 0xF;
+    GPIO_PMOD_OUT = 0;
+    GPIO_PMOD_OUT = out;
+    GPIO_PMOD_OUT = 0;
+}
+
+uint64_t get_cycles(void) {
+    uint32_t high;
+    uint32_t high2;
+    uint32_t low;
+    do {
+        asm volatile ("rdcycleh %0" : "=r" (high));
+        asm volatile ("rdcycle %0"  : "=r" (low));
+        asm volatile ("rdcycleh %0" : "=r" (high2));
+    } while (high != high2);
+    return ((uint64_t) high << 32) | low;
+}
+
+uint32_t get_timestamp_ms(void) {
+    // Note: assumes (16 * 1024 * 1024) Hz clock.
+    // Actually gives in units of 1.024ms.
+    return (uint32_t)(get_cycles() >> 14);
+}
+
+void recv_data(uint8_t* data, uint32_t count) {
+    while (count > 0) {
+        uint32_t avail = HOST_RX_COUNT;
+        uint32_t transfer = count > avail ? avail : count;
+        count -= transfer;
+
+        uint8_t* end = data + transfer;
+        while (data < end) {
+            *data = HOST_RX_DATA;
+            data++;
+        }
+    }
+}
+
+void send_byte(uint8_t data) {
+    while (HOST_TX_COUNT >= HOST_TX_MAX)
+        ;
+    HOST_TX_DATA = data;
+}
+
 void send_data(uint8_t* data, uint32_t count) {
     while (count > 0) {
         uint32_t avail = HOST_TX_MAX - HOST_TX_COUNT;
@@ -14,8 +61,25 @@ void send_data(uint8_t* data, uint32_t count) {
     }
 }
 
+uint8_t recv_byte() {
+    while (HOST_RX_COUNT == 0)
+        ;
+    return HOST_RX_DATA;
+}
+
+
 int main() {
-    char str[] = "hello, world!";
-    send_data((uint8_t*) str, sizeof(str));
+    while (1) {
+        if (HOST_RX_COUNT > 0) {
+            // A command byte is ready.
+            uint8_t command = HOST_RX_DATA;
+            lk_loop(command);
+            continue;
+        }
+
+        // Periodic updates
+        lk_runtime = get_timestamp_ms();
+        lk_cart_power_off_proc();
+    }
     return 0;
 }
